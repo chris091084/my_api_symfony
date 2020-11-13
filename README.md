9117a7eef255b6b0f507d8eb6ef98aa57010e3df
christian coley <christian.coley@apside-groupe.com>
2020-11-13 18:02:08 +0100

    add_routes

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # my_api_symfony
+
+```
+POST api/articles
+
+Post an article
+body:
+{
+    "title": "le corona-virus",
+    "content": "c'est de la vrais daube ce virus de merde"
+}
+```
+
+```
+GET /api/allArticlesSimple
+
+Get all articles
+Response:
+{
+        "id": 1,
+        "title": "1er article",
+        "content": "voici mon première article"
+    },
+```
+
+
+
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ArticleController extends AbstractController
 {
@@ -37,9 +40,6 @@ class ArticleController extends AbstractController
        $article->setTitle($data->title);
        $article->setContent($data->content);
 
-
-       //$article = $this->container->get('jms_serializer')->deserialize($data, 'AppBundle\Entity\Article', 'json');
-
        $em = $this->getDoctrine()->getManager();
        $em->persist($article);
        $em->flush();
@@ -47,41 +47,51 @@ class ArticleController extends AbstractController
        return new Response('', Response::HTTP_CREATED);
    }
 
-   /**
- * @Route("/allArticles", name="liste", methods={"GET"})
- */
-   public function getAllArticles(ArticleRepository $articleRepository){
-
+    /**
+     * @Route("/allArticlesNormalizer", name="listeNormalizer", methods={"GET"})
+     * @param ArticleRepository $articleRepositoryn
+     * @param NormalizerInterface $normalizerInterface
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+   public function getAllArticles(ArticleRepository $articleRepository, NormalizerInterface $normalizer ) :JsonResponse
+   {
     // on récupère les éléments de la base de donnée
     $articles = $articleRepository->findAll();
+    // on ne peut pas utliser dans ce cas la méthode jscon_encode car sinon on optient un tableau d'objet vide du fait que les attributs sont "private"
+    // de ce fait j'implémente dans ma méthode l'interface Normalizer. il faut bien dans ce cas créer des groupes dans l'entity pour dire quels sont les attribut à faire appraitre
+    //avec l'annotation @Groups("post:read")
 
-    // on spécifie qu'on utilise en encodeur en json 
-    $encoders = [new JsonEncoder()];
-
-    //on instancie le "normaliser" pour converti la collection  en tableau
-    $normalizers = [new ObjectNormalizer()];
-
-    // on fait la conversion json
-    // on instancie le convertisseur
-    $serializer = new Serializer($normalizers,$encoders);
-
-    //on convertie en Json
-    $jsonContent = $serializer->serialize($articles, 'json',[
-        'circular_reference_handler' => function($object){
-            return $object->getId();
-        }
-    ]);
-
-    //on instancie la réponse
-    $response = new Response($jsonContent);
-
-    // on ajoute l'entete HTTP
-    $response->headers->set('Content-type','application/json');
-    
-    // on envoie la réponse
-    return $response;
-    
+       $articleNormalizer = $normalizer->normalize($articles,null,[]);
+       $json = json_encode($articleNormalizer);
+    return $response = new Response($json, 200,
+    ['Content-Type' => 'application/json']
+    );
    }
 
-    
+    /**
+     * @Route("/allArticlesSerializer", name="listeSerialize", methods={"GET"})
+     * @param ArticleRepository $articleRepository
+     * @param SerializerInterface $serializer
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getAllArticlesSerializer(ArticleRepository $articleRepository, SerializerInterface $serializer ):JsonResponse
+    {
+        // on récupère les éléments de la base de donnée
+        $articles = $articleRepository->findAll();
+        $json = $serializer->serialize($articles,'json', []);
+        return $response = new JsonResponse($json, 200,true);
+    }
+
+    /**
+     * @Route("/allArticlesSimple", name="listeSimple", methods={"GET"})
+     * @param ArticleRepository $articleRepository
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getAllArticlesSimple(ArticleRepository $articleRepository ) :JsonResponse
+    {
+        // on récupère les éléments de la base de donnée
+        $articles = $articleRepository->findAll();
+        return $response = $this->json($articles, 200, [],[]);
+    }
+    //voir video https://www.youtube.com/watch?v=SG7GgcnR1F4&t=1501s
 }
